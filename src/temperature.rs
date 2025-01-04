6980341d64a43080b05b1b414934e7b2f38986bd
pijus <pijus.cezar13@gmail.com>
2025-01-04 11:03:09 +0200

    reading pulses from the sensor

--- src/temperature.rs
+++ src/temperature.rs
@@ -2,9 +2,12 @@
 use crate::cli_error::CliError;
 use crate::dht::*;
 
-use rppal::gpio::{Gpio, Level, Mode};
+use rppal::gpio::{Gpio, Level, Mode, IoPin};
 use std::thread::sleep;
 use std::time::Duration;
+
+const MAX_COUNT: usize = 32000;
+const DHT_PULSES: usize = 41;
 
 pub struct Temperature;
 
@@ -16,11 +19,13 @@ impl Temperature {
 
 pub trait ITemperature {
     fn read(&self, pin: u8) -> Result<(), CliError>;
+
+    fn read_pulses(&self, pin: &IoPin, level: Level) -> Result<usize, CliError>;
 }
 
 impl ITemperature for Temperature {
     fn read(&self, pin: u8) -> Result<(), CliError> {
-        let mut dht_pin: rppal::gpio::IoPin = Gpio::new()?.get(pin)?.into_io(Mode::Output);
+        let mut dht_pin = Gpio::new()?.get(pin)?.into_io(Mode::Output);
 
         dht_pin.write(Level::High);
         sleep(Duration::from_millis(500));
@@ -30,19 +35,33 @@ impl ITemperature for Temperature {
         dht_pin.set_mode(Mode::Input);
         tiny_sleep();
 
-        let mut count: usize = 0;
-
         // if the dht returns only high, the dht did not get proper data.
-        while dht_pin.read() == Level::High {
-            count = count + 1;
+        self.read_pulses(&dht_pin, Level::High)?;
 
-            if count > MAX_COUNT {
+        let mut pulse_counts: [usize; DHT_PULSES * 2] = [0; DHT_PULSES * 2];
+        
+        // read high and low pulses from the sensor
+        for c in 0..DHT_PULSES {
+            let i = c * 2;
+
+            pulse_counts[i] = self.read_pulses(&dht_pin, Level::Low)?;
+            pulse_counts[i + 1] = self.read_pulses(&dht_pin, Level::High)?;
+        }
+
+        println!("{:?}", pulse_counts);
+
+        Ok(())
+    }
+
+    fn read_pulses(&self, pin: &IoPin, level: Level) -> Result<usize, CliError> {
+        let mut pulse_count = 0;
+        while pin.read() == level {
+            pulse_count = pulse_count + 1;
+
+            if pulse_count > MAX_COUNT {
                 return Err(CliError::Error(String::from("Timeout")));
             }
         }
-
-        // read
-
-        Ok(())
+        Ok(pulse_count)
     }
 }
